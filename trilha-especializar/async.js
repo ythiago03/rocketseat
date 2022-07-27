7454dd74dc2e6c0744e247a53e52c66e74ba3518
Thiago Henrique <thiagohcfidencio@gmail.com>
2022-07-27 11:34:49 -0300

    testes

--- trilha-especializar/async.js
+++ trilha-especializar/async.js
@@ -16,6 +16,9 @@ const altura = document.querySelector('.altura')
 let div = document.querySelector('.div')
 const abili = document.querySelector('.abili')
 
+let teste = []
+
+
 const get3pokemon = async () => {
     const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/' + num)
     const name = pokemon.data.name
@@ -24,23 +27,27 @@ const get3pokemon = async () => {
     const height = pokemon.data.height
     const ability = pokemon.data.abilities
 
-
-    
-
-    p.textContent = name
+    p.textContent = `Nome: ${name}`
     img.setAttribute('src', urlImage)
-    peso.textContent = `${weight} Kg`
-    altura.textContent = `${height}0 cm`
+    peso.textContent = `Peso: ${weight} Kg`
+    altura.textContent = `Altura: ${height}0 cm`
 
 
-    for(let i = 0; i < ability.length; i++){
+
+    // for(let i = 0; i < ability.length; i++){
        
-        console.log(ability[i].ability.name);
-        abili.textContent = ability[i].ability.name
+    //     console.log(ability[i].ability.name);
+    //     abili.textContent = ability[i].ability.name
        
-    }
+    // }
 
+    ability.forEach((element, i) => {
+        teste.push(ability[i].ability.name)
 
+        abili.textContent = teste.join('___')
+    })
+
+    console.log(ability);
     console.log(pokemon.data);
 }
 
